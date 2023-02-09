21c5695ca28040d3cf0f89a39e20f02de3ecc359
mrlamba <mrlamba@protonmail.com>
2023-02-09 19:03:41 +0700

    adding new changes

---
+++ app/routes/index.tsx
@@ -0,0 +1,15 @@
+import { LoaderFunction } from '@remix-run/node'
+import { requireUserId } from '~/utils/auth.server'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request)
+  return null
+}
+
+export default function Index() {
+  return (
+    <div className="h-screen w-full bg-slate-600">
+      <h2 className="font-bol text-5xl text-blue-400">It works!</h2>
+    </div>
+  );
+}
